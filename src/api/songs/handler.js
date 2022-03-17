01638ac625e69de25373dacfa85ed3c1f4ac4704
naja1 <naja@ternakuang.id>
2022-03-17 07:18:33 +0700

    first commit

---
+++ src/api/songs/handler.js
@@ -0,0 +1,168 @@
+/* eslint-disable no-underscore-dangle */
+const ClientError = require('../../exceptions/ClientError');
+
+class SongsHandler {
+  constructor(service, validator) {
+    this._service = service;
+    this._validator = validator;
+
+    this.postSongHandler = this.postSongHandler.bind(this);
+    this.getSongsHandler = this.getSongsHandler.bind(this);
+    this.getSongByIdHandler = this.getSongByIdHandler.bind(this);
+    this.putSongByIdHandler = this.putSongByIdHandler.bind(this);
+    this.deleteSongByIdHandler = this.deleteSongByIdHandler.bind(this);
+  }
+
+  async postSongHandler(request, h) {
+    try {
+      this._validator.validateSongPayload(request.payload);
+      const {
+        title, year, performer, genre, duration, albumId,
+      } = request.payload;
+
+      const songId = await this._service.addSong({
+        title, year, performer, genre, duration, albumId,
+      });
+
+      const response = h.response({
+        status: 'success',
+        message: 'Song Added Successully',
+        data: {
+          songId,
+        },
+      });
+      response.code(201);
+      return response;
+    } catch (error) {
+      if (error instanceof ClientError) {
+        const response = h.response({
+          status: 'fail',
+          message: error.message,
+        });
+        response.code(error.statusCode);
+        return response;
+      }
+
+      // Server ERROR!
+      const response = h.response({
+        status: 'error',
+        message: 'Sorry, something went wrong in the server.',
+      });
+      response.code(500);
+      console.error(error);
+      return response;
+    }
+  }
+
+  async getSongsHandler(request) {
+    const { title, performer } = request.query;
+    const query = {};
+    if (title) {
+      query.title = title;
+    }
+    if (performer) {
+      query.performer = performer;
+    }
+    const songs = await this._service.getSongs(query);
+    return {
+      status: 'success',
+      data: {
+        songs,
+      },
+    };
+  }
+
+  async getSongByIdHandler(request, h) {
+    try {
+      const { id } = request.params;
+      const song = await this._service.getSongById(id);
+      return {
+        status: 'success',
+        data: {
+          song,
+        },
+      };
+    } catch (error) {
+      if (error instanceof ClientError) {
+        const response = h.response({
+          status: 'fail',
+          message: error.message,
+        });
+        response.code(error.statusCode);
+        return response;
+      }
+
+      // Server ERROR!
+      const response = h.response({
+        status: 'error',
+        message: 'Sorry, something went wrong in the server.',
+      });
+      response.code(500);
+      console.error(error);
+      return response;
+    }
+  }
+
+  async putSongByIdHandler(request, h) {
+    try {
+      this._validator.validateSongPayload(request.payload);
+      const { id } = request.params;
+
+      await this._service.editSongById(id, request.payload);
+
+      return {
+        status: 'success',
+        message: 'Song updated successfully',
+      };
+    } catch (error) {
+      if (error instanceof ClientError) {
+        const response = h.response({
+          status: 'fail',
+          message: error.message,
+        });
+        response.code(error.statusCode);
+        return response;
+      }
+
+      // Server ERROR!
+      const response = h.response({
+        status: 'error',
+        message: 'Sorry, something went wrong in the server.',
+      });
+      response.code(500);
+      console.error(error);
+      return response;
+    }
+  }
+
+  async deleteSongByIdHandler(request, h) {
+    try {
+      const { id } = request.params;
+      await this._service.deleteSongById(id);
+      return {
+        status: 'success',
+        message: 'Song deleted successfully',
+      };
+    } catch (error) {
+      if (error instanceof ClientError) {
+        const response = h.response({
+          status: 'fail',
+          message: error.message,
+        });
+        response.code(error.statusCode);
+        return response;
+      }
+
+      // Server ERROR!
+      const response = h.response({
+        status: 'error',
+        message: 'Sorry, something went wrong in the server.',
+      });
+      response.code(500);
+      console.error(error);
+      return response;
+    }
+  }
+}
+
+module.exports = SongsHandler;
---
+++ src/utils/index.js
@@ -0,0 +1,38 @@
+/* eslint-disable camelcase */
+const mapDBToModelSongs = ({
+  id,
+  title,
+  year,
+  performer,
+  genre,
+  duration,
+  album_id,
+  created_at,
+  updated_at,
+}) => ({
+  id,
+  title,
+  year,
+  performer,
+  genre,
+  duration,
+  albumId: album_id,
+  createdAt: created_at,
+  updatedAt: updated_at,
+});
+
+const mapDBToModelAlbums = ({
+  id,
+  name,
+  year,
+  created_at,
+  updated_at,
+}) => ({
+  id,
+  name,
+  year,
+  createdAt: created_at,
+  updatedAt: updated_at,
+});
+
+module.exports = { mapDBToModelSongs, mapDBToModelAlbums };
